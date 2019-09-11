3cf7087b9e6f8c23fdb6854e2a0afa27b4f7acda
Andreas Fahrecker <andreasfahrecker@gmail.com>
2019-09-11 19:12:22 +0200

    Created Project
    Singed-off: Andreas Fahrecker <andreasfahrecker@gmail.com>

---
+++ sinusbot-onlinestatus-subscriber.js
@@ -0,0 +1,10 @@
+registerPlugin({
+        name: 'Sinusbot-Onlinestatus-Subsriber',
+        version: '1.0',
+        description: '',
+        author: 'Andreas Fahrecker <andreasfahrecker@gmail.com>',
+        vars: []
+    },
+    function (_, config, meta) {
+
+    });
